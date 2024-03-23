006e8ff30afabaee1bf7154e9772816d8ad551d7
alistairjkho <alistairjkho@gmail.com>
2024-03-23 00:04:10 +0000

    update

--- pages/6_Pygwalker_file_upload.py
+++ pages/6_Pygwalker_file_upload.py
@@ -54,11 +54,12 @@ init_streamlit_comm()
 
 # Get an instance of pygwalker's renderer. You should cache this instance to effectively prevent the growth of in-process memory.
 @st.cache_resource
-def get_pyg_renderer(df) -> "StreamlitRenderer":
+def get_pyg_renderer() -> "StreamlitRenderer":
+    df = load_data(uploaded_file)
     # When you need to publish your app to the public, you should set the debug parameter to False to prevent other users from writing to your chart configuration file.
     return StreamlitRenderer(df, spec="./gw_config.json", debug=False)
  
 if st.button('Click me!'):
-    renderer = get_pyg_renderer(df)
+    renderer = get_pyg_renderer()
     # Render your data exploration interface. Developers can use it to build charts by drag and drop.
     renderer.render_explore()
